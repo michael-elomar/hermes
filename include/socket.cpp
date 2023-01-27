3d513d7366e287bdb9ae73de644cc0db54135ab7
Michael El Omar <31253480+quis-ut-deus@users.noreply.github.com>
2023-01-27 12:30:27 +0100

    add new Socket() constructors

--- include/socket.cpp
+++ include/socket.cpp
@@ -17,5 +17,71 @@ namespace hermes
             std::cerr << "Failed to initialize socket" << std::endl;
             exit(EXIT_FAILURE);
         }
+
+        this->socket_type = socket_type;
+    }
+    Socket::Socket(uint8_t comm_type, uint8_t socket_type, IPAddress ip_address)
+    {
+        if (comm_type == CommType::TCP)
+            this->socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+        else
+            this->socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+        if (this->socket_fd < 0)
+        {
+            std::cerr << "Failed to initialize socket" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+        this->socket_type = socket_type;
+
+        int opt = 1;
+        if (setsockopt(this->socket_fd, SOL_SOCKET, SO_REUSEPORT , &opt, sizeof(opt)) < 0)
+        {
+            printf("Failure to perform setsockopt\n");
+            perror("setsockopt");
+            exit(EXIT_FAILURE);
+        }
+
+        if (setsockopt(this->socket_fd, SOL_SOCKET, SO_REUSEADDR , &opt, sizeof(opt)) < 0)
+        {
+            printf("Failure to perform setsockopt\n");
+            perror("setsockopt");
+            exit(EXIT_FAILURE);
+        }
+
+        struct sockaddr_in sock_addr = ip_address.GetSockAddr();
+        if (int code = bind(this->socket_fd,
+            (struct sockaddr *) &sock_addr, sizeof(sock_addr)) < 0)
+        {
+            printf("Bind operation failed\nError code %d\n", code);
+            exit(EXIT_FAILURE);
+        }
+    }
+    bool Socket::Bind(IPAddress ip_address)
+    {
+        int opt = 1;
+        if (setsockopt(this->socket_fd, SOL_SOCKET, SO_REUSEPORT , &opt, sizeof(opt)) < 0)
+        {
+            printf("Failure to perform setsockopt\n");
+            perror("setsockopt");
+            exit(EXIT_FAILURE);
+        }
+
+        if (setsockopt(this->socket_fd, SOL_SOCKET, SO_REUSEADDR , &opt, sizeof(opt)) < 0)
+        {
+            printf("Failure to perform setsockopt\n");
+            perror("setsockopt");
+            exit(EXIT_FAILURE);
+        }
+
+        struct sockaddr_in sock_addr = ip_address.GetSockAddr();
+        if (int code = bind(this->socket_fd,
+            (struct sockaddr *) &sock_addr, sizeof(sock_addr)) < 0)
+        {
+            printf("Bind operation failed\nError code %d\n", code);
+            exit(EXIT_FAILURE);
+        }
+
+        return true;
     }
 }
