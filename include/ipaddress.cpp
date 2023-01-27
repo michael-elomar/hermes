0509c8073c93296bc428e8b831b04969e84aa84e
Michael El Omar <31253480+quis-ut-deus@users.noreply.github.com>
2023-01-27 12:31:09 +0100

    Remove useless IPAddress() constructors

--- include/ipaddress.cpp
+++ include/ipaddress.cpp
@@ -5,19 +5,9 @@
 
 namespace hermes
 {
-    uint32_t IPAddress::inet_addr(uint8_t *address_array, uint8_t size)
+    in_addr_t IPAddress::inet_addr(std::string address)
     {
-        uint32_t ip_addr_value = 0;
-        for(int i=0; i < size; i++)
-        {
-            // printf("%d\n%d\n", address_array[i], size-i-1);
-            ip_addr_value += address_array[i] * pow((double)256, (double)(size - i - 1));
-        }
-        return ip_addr_value;
-    }
-    uint32_t IPAddress::inet_addr(std::string address)
-    {
-        uint32_t ip_addr_value = 0, i = 3;
+        in_addr_t ip_addr_value = 0, i = 3;
         std::string delimiter = ".";
         while (address.find(delimiter) != std::string::npos) {
             std::string token = address.substr(0, address.find(delimiter));
@@ -28,40 +18,25 @@ namespace hermes
         ip_addr_value += std::stoi(address) * pow(256, i);
         return ip_addr_value;
     }
-    IPAddress::IPAddress(uint8_t *address_array, uint8_t size)
-    {
-        this->sock_addr.sin_family = AF_INET; //IPv4 by default
-        this->sock_addr.sin_addr.s_addr = IPAddress::inet_addr(address_array, 4);
-        this->sock_addr.sin_port = htons(DEFAULT_PORT);
-
-        std::stringstream ss;
-        for(int i=0;i < size - 1;i++)
-        {
-            ss << std::to_string(address_array[i]) << ".";
-        }
-        ss << std::to_string(address_array[size-1]);
-        this->address_str = ss.str();
-    }
-    IPAddress::IPAddress(uint8_t *address_array, uint8_t size, uint32_t port)
-    {
-        this->sock_addr.sin_family = AF_INET; //IPv4 by default
-        this->sock_addr.sin_addr.s_addr = IPAddress::inet_addr(address_array, size);
-        this->sock_addr.sin_port = htons(port);
-    }
+    IPAddress::IPAddress() {}
 
     IPAddress::IPAddress(std::string address)
     {
         this->sock_addr.sin_family = AF_INET; //IPv4 by default
-        this->sock_addr.sin_addr.s_addr = IPAddress::inet_addr(address);
+        this->sock_addr.sin_addr.s_addr = htonl(IPAddress::inet_addr(address));
         this->sock_addr.sin_port = htons(DEFAULT_PORT);
 
         this->address_str = address;
+        this->port = DEFAULT_PORT;
     }
     IPAddress::IPAddress(std::string address, uint32_t port)
     {
         this->sock_addr.sin_family = AF_INET; //IPv4 by default
-        this->sock_addr.sin_addr.s_addr = IPAddress::inet_addr(address);
+        this->sock_addr.sin_addr.s_addr = htonl(IPAddress::inet_addr(address));
         this->sock_addr.sin_port = htons(port);
+
+        this->address_str = address;
+        this->port = port;
     }
     std::string IPAddress::GetIPAddress()
     {
@@ -71,7 +46,7 @@ namespace hermes
     {
         return this->port;
     }
-    sockaddr_in IPAddress::GetSockAddr()
+    struct sockaddr_in IPAddress::GetSockAddr()
     {
         return this->sock_addr;
     }
